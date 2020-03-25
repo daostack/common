dc7d8e42f459510750f4fc109d81393c2835fd4a
lmcmz <lmcmze@gmail.com>
2020-03-25 18:58:23 +1100

    Add storedMnemonic bridge in iOS

--- src/Util/NativeWallet.js
+++ src/Util/NativeWallet.js
@@ -1,32 +1,52 @@
-import { NativeModules } from "react-native";
-import { ethers } from 'ethers';
+import {NativeModules} from 'react-native';
+import {ethers} from 'ethers';
 
 const generateMnemonic = async () => {
-    try {
-        return await NativeModules.WalletModule.generateMnemonic();
-    } catch (e) {
-        console.log(e);
-    }
+  try {
+    return await NativeModules.WalletModule.generateMnemonic();
+  } catch (e) {
+    console.log(e);
+  }
+};
+
+const generateAndStoreMnemonic = async () => {
+  try {
+    return await NativeModules.WalletModule.generateAndStoreMnemonic();
+  } catch (e) {
+    console.log(e);
+  }
+};
+
+const storeMnemonic = async mnemonic => {
+  try {
+    return await NativeModules.WalletModule.storeMnemonic(mnemonic);
+  } catch (e) {
+    console.log(e);
+  }
 };
 
 const retrieveMnemonic = async () => {
-    try {
-        return await NativeModules.WalletModule.retrieveMnemonic();
-    } catch (e) {
-        console.log(e);
-    }
+  try {
+    return await NativeModules.WalletModule.retrieveMnemonic();
+  } catch (e) {
+    console.log(e);
+  }
 };
 
-const signMessage = async (message) => {
-    try {
-        return await NativeModules.WalletModule.signMessage(ethers.utils.formatBytes32String(message));
-    } catch (e) {
-        throw 'Sign message failed with error: ' + e;
-    }
+const signMessage = async message => {
+  try {
+    return await NativeModules.WalletModule.signMessage(
+      ethers.utils.formatBytes32String(message),
+    );
+  } catch (e) {
+    throw 'Sign message failed with error: ' + e;
+  }
 };
 
 export const NativeWallet = {
-    generateMnemonic,
-    retrieveMnemonic,
-    signMessage
+  generateMnemonic,
+  generateAndStoreMnemonic,
+  storeMnemonic,
+  retrieveMnemonic,
+  signMessage,
 };
