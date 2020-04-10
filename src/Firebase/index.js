6cab51842eef8c07f29e1d6af49974aa0a82a8d2
lmcmz <lmcmze@gmail.com>
2020-04-10 19:55:22 +1000

    FIx android build problem

--- src/Firebase/index.js
+++ src/Firebase/index.js
@@ -2,10 +2,6 @@ import firebase from '@react-native-firebase/app';
 import firestore from '@react-native-firebase/firestore';
 import messaging from '@react-native-firebase/messaging';
 
-const {firebaseConfig} = require('../../env');
-
-firebase.initializeApp(firebaseConfig);
-
 module.exports = {
   db: firestore,
   messaging: messaging,
