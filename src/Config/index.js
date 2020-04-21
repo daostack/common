14a188065f5c5f4694a6068d3a1218bd6c70e230
Mark Pereira <markpereira17@gmail.com>
2020-04-21 06:24:15 -0400

    fixing linter

--- src/Config/index.js
+++ src/Config/index.js
@@ -8,9 +8,9 @@ import {WebSocketLink} from 'apollo-link-ws';
 import {getMainDefinition} from 'apollo-utilities';
 
 const graphHttpLink =
-  'https://api.thegraph.com/subgraphs/name/daostack/v7_2_exp_rinkeby';
+  'https://api.thegraph.com/subgraphs/name/daostack/v7_4_exp_rinkeby';
 const graphwsLink =
-  'wss://api.thegraph.com/subgraphs/name/daostack/v7_2_exp_rinkeby';
+  'wss://api.thegraph.com/subgraphs/name/daostack/v7_4_exp_rinkeby';
 const ipfsLink = 'https://api.thegraph.com/ipfs-daostack/api/v0';
 
 const httpLink = new HttpLink({
