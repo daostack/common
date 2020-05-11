307f179ef585bdb34fc818d9e8d65773ad14aab3
Jelle <jellegerbrandy@gmail.com>
2020-05-11 17:09:57 +0200

    more linting

--- src/Screens/CommonProfile.js
+++ src/Screens/CommonProfile.js
@@ -21,7 +21,7 @@ import SortProposals from './BottomSheetScreens/SortProposals';
 import CommonProfileOptions from './BottomSheetScreens/CommonProfileOptions';
 //import ProposalSheetScreen from './Proposals/ProposalSheetScreen';
 import ProposalSheetScreen from './BottomSheetScreens/ProposalSheetScreen';
-import ApprovalSheetScreen from './BottomSheetScreens/ApprovalSheetScreen';
+// import ApprovalSheetScreen from './BottomSheetScreens/ApprovalSheetScreen';
 
 import BottomSheetContainer from '../Components/BottomSheetContainer';
 import CommonCover from '../Components/Commons/CommonCover';
--- src/Screens/Commons/RequestStepNavigation.js
+++ src/Screens/Commons/RequestStepNavigation.js
@@ -1,4 +1,3 @@
-/* eslint-disable prettier/prettier */
 import React from 'react';
 import {TouchableOpacity} from 'react-native';
 import NavigationBar from 'react-native-navbar';
