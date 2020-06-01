e46849e35e77807c0721e00927d81eacd6a36d43
lpetkov-sw <l.petkov@shopdow.com>
2020-06-01 08:49:02 +0300

    Fixed bug in create step 3

--- src/FormStores/index.js
+++ src/FormStores/index.js
@@ -1,9 +1,7 @@
 import CompleteAccountFormStore from './CompleteAccountFormStore';
-import CreateCommonFormStore from './CreateCommonFormStore';
 import FundingRequestFormStore from './FundingRequestFormStore';
 
 export default {
   completeAccountFormStore: new CompleteAccountFormStore(),
-  createCommonFormStore: new CreateCommonFormStore(),
   fundingRequestFormStore: new FundingRequestFormStore(),
 };
