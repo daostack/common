a09702d1decd73eb43e346309c42ca15d530f118
Anton <anton@Antons-Mac-mini.local>
2020-06-03 14:55:26 +0300

    send minimum and funding to IPFS in cents, pass cents to arc too

--- src/Screens/CreateCommon/CreateStep4.js
+++ src/Screens/CreateCommon/CreateStep4.js
@@ -120,19 +120,28 @@ const CreateStep4 = props => {
         // TODO: actuall add the values here (as an arry probably)
         rules: formData.rules,
         links: formData.links,
+        minimum: formData.minimum,
+        funding: formData.funding,
       }),
     );
   };
 
   const forgeCommon = async () => {
-    const formData = {
+    const formDataInit = {
       ...props.generalInfoFormStore.getChangedFormFieldsJson(),
       ...props.fundingFormStore.getChangedFormFieldsJson(),
       ...props.agendaFormStore.getChangedFormFieldsJson(),
       ...props.reviewFormStore.getChangedFormFieldsJson(),
     };
 
+    const formData = {
+      ...formDataInit,
+      minimum: parseInt(formDataInit.minimum, 10) * 100,
+      funding: parseInt(formDataInit.funding, 10) * 100,
+    };
+
     console.log('saving data on ipfs: ', formData);
+
     const ipfsHash = await ipfsUpload(formData);
     const manager = await WalletManager.getInstance();
     const address = await manager.getAddress();
@@ -145,8 +154,8 @@ const CreateStep4 = props => {
       founderAddresses: address,
       tokenDist: [0],
       repDist: [1000],
-      minFeeToJoin: parseInt(formData.minimum, 10) * 100, // multiply by 100 to get the value in cents
-      fundingGoal: parseInt(formData.funding, 10) * 100, // multiply by 100 to get the value in cents
+      minFeeToJoin: formData.minimum,
+      fundingGoal: formData.funding,
       fundingGoalDeadline: Math.round(deadline / 1000),
       ipfsHash,
     };
