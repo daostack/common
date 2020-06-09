51be634092d06f984bbc7159447e2bc59e66f0ce
Anton <anton@Antons-Mac-mini.local>
2020-06-09 18:28:25 +0300

    no-undef turned on

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,11 +1,8 @@
 module.exports = {
   root: true,
-  extends: [
-    '@react-native-community/eslint-config'
-  ],
+  extends: ['@react-native-community/eslint-config'],
   rules: {
     'react-native/no-inline-styles': 'off',
-    'no-undef': 'off',
     'no-shadow': 0,
     'react-hooks/exhaustive-deps': 0,
     'prettier/prettier': 0,
