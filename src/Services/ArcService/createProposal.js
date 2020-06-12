2efb6d65e3ca9453feed2a74bc4bf2d71ecbf4a7
jelle <jellegerbrandy@gmail.com>
2020-06-12 12:53:26 +0200

    fix error

--- src/Services/ArcService/createProposal.js
+++ src/Services/ArcService/createProposal.js
@@ -57,17 +57,17 @@ export const createProposalRequestToJoin = async (arc, daoId, data) => {
       const joinAndQuitContract  = await arc.getContract(joinAndQuitPlugin.coreState.address);
       const proposer =   WalletManager.getInstance().safeAddress;
       // we check the conditions from the contract
+
       // require(!fundings[proposer].candidate, "already a candidate");
       const memberFund = await joinAndQuitContract.fundings(proposer);
       if (memberFund[0] === true) {
-        throw Error(`The proposer ${proposer} is already a canidate - cannot create a request`);
+        // If this error is thrown from a user action, there is a ui bug:s it means that some action was enabled where it shoudl not
+        throw Error(`Cannot create the proposal, because the proposer ${proposer} has created such a request before`);
       }
 
       // TODO: check the other conditions
       // require(avatar.nativeReputation().balanceOf(proposer) == 0, "already a member");
       // require(_feeAmount >= minFeeToJoin, "_feeAmount should be >= then the minFeeToJoin")
-
-      throw Error('Oh no');
     };
     await errorHandler();
     const transaction = await joinAndQuitPlugin.createProposalTransaction(args);
