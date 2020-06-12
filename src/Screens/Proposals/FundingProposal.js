74e039a20a1a88496c95b13ecb6bcfb72d790ccc
lpetkov-sw <l.petkov@shopdow.com>
2020-06-12 17:09:46 +0300

    Added funding request action to the Create Proposal button.

--- src/Screens/Proposals/FundingProposal.js
+++ src/Screens/Proposals/FundingProposal.js
@@ -10,9 +10,17 @@ import {observer, inject} from 'mobx-react';
 import {text, layout, colors} from '../../Theme';
 import FundingRequestForm from '../../Components/Forms/FundingRequestForm';
 import RequestStepActionButton from '../Commons/RequestStepActionButton';
-import {CommonActions} from '@react-navigation/native';
+import { CommonActions } from '@react-navigation/native';
+import ArcService from '../../Services/ArcService';
+import { BN } from 'bn.js';
+import Toast from '../../Util/Toast';
 
-const FundingProposal = ({fundingRequestFormStore, navigation}) => {
+const FundingProposal = ({
+  userStore,
+  fundingRequestFormStore,
+  navigation,
+  route,
+}) => {
   // TODO: can these lines be removed?
   // const viewProposal = () => {
   //   //navigation.navigate('RequestStep1');
@@ -22,8 +30,45 @@ const FundingProposal = ({fundingRequestFormStore, navigation}) => {
   //   setShowRequestSentModal(false);
   // };
 
-  const createProposal = e => {
-    //setShowRequestSentModal(true);
+  const createProposal = async e => {
+    if (fundingRequestFormStore.isFormValid()) {
+      try {
+        const formData = fundingRequestFormStore.getChangedFormFieldsJson();
+        console.log('FORM DATA', formData);
+        const data = {
+          title: formData[FundingRequestForm.FIELD_TITLE],
+          description: formData[FundingRequestForm.FIELD_DESCRIPTION],
+          funding: new BN(formData[FundingRequestForm.FIELD_AMOUNT_REQUESTED] * 100,),
+          links: formData[FundingRequestForm.FIELD_LINKS],
+          images: formData[FundingRequestForm.FIELD_IMAGES],
+          files: formData[FundingRequestForm.FIELD_FILES],
+        };
+
+        console.log('DATA -> ', data);
+        Toast.loading('Creating funding proposal...');
+
+        const proposalId = await ArcService.getInstance().createFundingProposal(
+          userStore.userInfo.safeAddress,
+          route.params.commonId,
+          data,
+        );
+        Toast.hide();
+        Toast.done(`Funding Proposal with id ${proposalId} created!`);
+
+        const navigate = CommonActions.navigate({
+          name: 'CommonProfile',
+          params: {
+            showRequestSentModal: true,
+            createdProposalId: proposalId,
+          },
+        });
+        navigation.dispatch(navigate);
+
+      } catch (error) {
+        console.log(error);
+        Toast.error(error.toString());
+      }
+    }
 
     const navigate = CommonActions.navigate({
       name: 'CommonProfile',
@@ -32,6 +77,7 @@ const FundingProposal = ({fundingRequestFormStore, navigation}) => {
       },
     });
     navigation.dispatch(navigate);
+    //setShowRequestSentModal(true);
   };
   return (
     <>
@@ -76,4 +122,7 @@ const styles = StyleSheet.create({
   },
 });
 
-export default inject('fundingRequestFormStore')(observer(FundingProposal));
+export default inject(
+  'userStore',
+  'fundingRequestFormStore',
+)(observer(FundingProposal));
