60447e44bd4ec21f9dc7ed8d3889eaf6350d5b2e
Anton <anton@Antons-Mac-mini.local>
2020-06-16 10:58:36 +0300

    fix images url

--- src/Screens/Commons/CommonsList.js
+++ src/Screens/Commons/CommonsList.js
@@ -39,7 +39,7 @@ const CommonsList = ({navigation, daoStore, bottomSheetStore, userStore}) => {
               ...{id: doc.id},
               ...doc.data(),
               ...{
-                coverPhoto: `https://i.picsum.photos/id/${index *
+                coverPhoto: `https://picsum.photos/id/${index *
                   10}/500/100.jpg`,
               },
             };
