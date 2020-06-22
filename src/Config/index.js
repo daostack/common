2af4b7b4b9f7ef0ccaa4b07a158ef8e8aed043ab
jelle <jellegerbrandy@gmail.com>
2020-06-22 14:04:49 +0200

    use v8_6 of the subgraph

--- src/Config/index.js
+++ src/Config/index.js
@@ -10,7 +10,7 @@ import {getMainDefinition} from 'apollo-utilities';
 // the value of ARC_VERSION should coincide with the "migration-experimental" versoin
 // TODO: we should probably read this from the package..
 export const ARC_VERSION = '0.1.1-rc.23';
-export const GRAPH_VERSION = 'v8_5_exp_xdai';
+export const GRAPH_VERSION = 'v8_6_exp_xdai';
 export const graphHttpLink = `https://api.thegraph.com/subgraphs/name/daostack/${GRAPH_VERSION}`;
 export const graphwsLink = `wss://api.thegraph.com/subgraphs/name/daostack/${GRAPH_VERSION}`;
 export const ipfsLink = 'https://api.thegraph.com/ipfs-daostack/api/v0';
