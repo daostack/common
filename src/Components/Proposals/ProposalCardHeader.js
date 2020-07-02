6090ba770f32cd240b1347915c9b078290b0a5b9
Anton <anton@Antons-Mac-mini.local>
2020-07-02 11:59:59 +0300

    check if has passed expiry date and set failed if so

--- src/Components/Proposals/ProposalCardHeader.js
+++ src/Components/Proposals/ProposalCardHeader.js
@@ -4,7 +4,7 @@ import {text, layout, colors, sizeXS} from '../../Theme';
 import Icon from '../../Assets/iconfont/Icon';
 import { LAUNCHED_STATES, COUNTDOWN_STATES, PROPOSAL_STAGE } from '../../Services/ProposalService';
 
-const calcIcon = (stage, winningOutcome) => {
+const calcIcon = (stage, winningOutcome, hasPassedExpiryDate) => {
   if (LAUNCHED_STATES.includes(stage)) {
     return 'boosted';
   }
@@ -13,34 +13,34 @@ const calcIcon = (stage, winningOutcome) => {
   }
 };
 
-const calcText = (stage, winningOutcome) => {
+const calcText = (stage, winningOutcome, hasPassedExpiryDate) => {
   if (LAUNCHED_STATES.includes(stage)) {
     return 'Launched';
   }
   if (COUNTDOWN_STATES.includes(stage)) {
     return 'Countdown';
   }
-
   if (stage === PROPOSAL_STAGE.Executed) {
     return winningOutcome === 1 ?  'Passed' : 'Failed';
   }
-
   if (stage === PROPOSAL_STAGE.ExpiredInQueue) {
+    return 'Failed';
+  }
+  if (hasPassedExpiryDate) {
     return 'Failed';
   }
 };
 
 
-const ProposalCardHeader = ({stage, winningOutcome}) => {
-
+const ProposalCardHeader = ({ stage, winningOutcome, hasPassedExpiryDate }) => {
 
   let iconColor = LAUNCHED_STATES.includes(stage) ? colors.mainBlue : colors.countdown;
 
   return (
     <View style={styles.proposalCardHeader}>
-      <Icon name={calcIcon(stage, winningOutcome)} color={iconColor} size={16} />
+      <Icon name={calcIcon(stage, winningOutcome, hasPassedExpiryDate)} color={iconColor} size={16} />
       <Text style={{ ...text.orangeSmallBold, ...{ marginHorizontal: 5 }, ...LAUNCHED_STATES.includes(stage) ? styles.launchedColor : styles.countdownColor }}>
-        {calcText(stage, winningOutcome)}
+        {calcText(stage, winningOutcome, hasPassedExpiryDate)}
       </Text>
     </View>
   );
