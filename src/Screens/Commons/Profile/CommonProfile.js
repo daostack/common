6b9d8483a9f252a043c6c7481ba4e13013c69925
Anton Petrov <tpettrov@gmail.com>
2020-07-12 11:55:28 +0300

    catch the case that DAO cannot be found (#281)
    Co-authored-by: Anton <anton@Antons-Mac-mini.local>

--- src/Screens/Commons/Profile/CommonProfile.js
+++ src/Screens/Commons/Profile/CommonProfile.js
@@ -34,6 +34,7 @@ import CountDown from 'react-native-countdown-component';
 import moment from 'moment';
 import {calcIsFundingStage} from '../../../Util';
 import firestore from '@react-native-firebase/firestore';
+import  Toast  from '../../../Util/Toast';
 import {
   Placeholder,
   PlaceholderMedia,
@@ -86,7 +87,12 @@ const CommonProfile = ({navigation, route, bottomSheetStore, userStore}) => {
         .collection('daos')
         .doc(commonId)
         .onSnapshot(snapshot => {
-          setCurrCommon(snapshot.data());
+          if (snapshot.exists) {
+            setCurrCommon(snapshot.data());
+          } else {
+            Toast.error('This DAO cannot be found try again later');
+            navigation.pop();
+          }
         });
       return unsubscribe;
     }
