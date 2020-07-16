549fb2d04ee27eb2b3fedcc5a0f40ab7bfb4f862
Jelle <jellegerbrandy@gmail.com>
2020-07-16 15:04:46 +0200

    Update CreateStep4.js
    fix lint warning

--- src/Screens/Commons/CreateCommon/CreateStep4.js
+++ src/Screens/Commons/CreateCommon/CreateStep4.js
@@ -78,7 +78,8 @@ const CreateStep4 = props => {
   const [imageURI, setImageURI] = useState(
     getImageUrl(1 + Math.floor(Math.random() * Math.floor(7))),
   );
-  const [avatarURL, setAvatarURL] = useState(null);
+  /* [avatarURL, setAvatarURL] = useState(null);
+  */
 
   //set default value for Avatar and Image fields
   useEffect(() => {
