276fc77fe7280cbf9a301985edd01bf5b514ca75
Anton Petrov <tpettrov@gmail.com>
2020-07-16 15:34:48 +0300

    Cm 425 card details direct to mangopay (#288)
    * add mangopay service
    
    * divide mangopay from create req to join
    
    * refactor
    
    Co-authored-by: Anton <anton@Antons-Mac-mini.local>

---
+++ src/Services/MangopayService.js
@@ -0,0 +1,61 @@
+import axios from 'axios';
+import { mangoPayUrl } from '../Config';
+import auth from '@react-native-firebase/auth';
+const qs = require('qs');
+
+
+const axiosClient = axios.create({
+  baseURL: mangoPayUrl,
+  // or for development:
+  //baseURL: 'http://localhost:5001/common-daostack/us-central1/mangopay/',
+  timeout: 1000000, // milliseconds
+});
+
+export const preauthorizePayment = async (cardData, funding) => {
+  try {
+    const idToken = await auth().currentUser.getIdToken();
+    // first create the user in mangopay if isn't already created
+    await axiosClient.post('create-user', { idToken });
+    // then get card pre-registration data
+    const { data: { preRegData}} = await axiosClient.post(
+      'get-card-registration',
+      { idToken },
+    );
+
+    const {
+      Id,
+      PreregistrationData,
+      AccessKey,
+      CardRegistrationURL,
+    } = preRegData;
+
+    const cardInfo = {
+      data: PreregistrationData,
+      accessKeyRef: AccessKey,
+      cardNumber: cardData.cardNumber,
+      cardExpirationDate: cardData.expDate,
+      cardCvx: cardData.cvv,
+    };
+    // post card sensitive data directly to tokenization server
+    const cardRegistrationData = await axios.post(CardRegistrationURL, qs.stringify(cardInfo), {
+      headers: {
+        'Content-Type': 'application/x-www-form-urlencoded',
+      },
+    });
+    // finalize the card registration - save cardId to firebase and preauthorize payment
+    const {data: {preAuthData: {preAuthId}}} = await axiosClient.post('register-card',
+      { idToken, cardRegistrationData, Id, funding  }
+    );
+    return preAuthId;
+  } catch (e) {
+    console.log(e);
+    console.log(e.response);
+    throw e;
+  }
+};
+
+
+
+/* export const registerCard = async () => {
+
+}; */
