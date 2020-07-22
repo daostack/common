9d01aeda3bd2212c42dbd398f0b1fd922955c724
lpetkov-sw <lyubomir.petkov@limechain.tech>
2020-07-22 16:07:47 +0300

    Added proposal activation date card. (#320)
    * Added proposal activation date card.
    
    * removed empty lines
    
    Co-authored-by: jelle <jellegerbrandy@gmail.com>

---
+++ src/Components/Proposals/ProposalActivationDate.js
@@ -0,0 +1,66 @@
+import React from 'react';
+import {Text, StyleSheet, View} from 'react-native';
+import {text, layout, colors} from '../../Theme';
+import Icon from '../../Assets/iconfont/Icon';
+
+import moment from 'moment';
+import {TouchableOpacity} from 'react-native-gesture-handler';
+
+const ProposalActivationDate = ({activationDate}) => {
+  const deadlineMoment = moment.unix(activationDate - 3600 * 38);
+  const deadlineHasPassed = moment().isAfter(deadlineMoment);
+
+  return !deadlineHasPassed ? (
+    <View style={styles.container}>
+      <Text style={styles.title}>Safety period</Text>
+      <Text style={styles.subtitle}>You will be able to create proposals</Text>
+      <Text style={styles.timeLeftStyle}>
+        {!deadlineHasPassed ? deadlineMoment.fromNow() : ''}
+      </Text>
+      <TouchableOpacity style={styles.explanationBtn}>
+        <Icon
+          name="explanation1"
+          color={colors.mainBlue}
+          style={layout.marginRightS}
+        />
+        <Text style={styles.explanationBtnTextStyle}>What's this?</Text>
+      </TouchableOpacity>
+    </View>
+  ) : null;
+};
+
+const styles = StyleSheet.create({
+  container: {
+    ...layout.content,
+
+    borderWidth: 0,
+    borderRadius: 28,
+    backgroundColor: colors.iceBlue,
+  },
+  subtitle: {
+    ...text.h2Black,
+    fontWeight: 'normal',
+    textAlign: 'center',
+
+    ...layout.marginTopS,
+  },
+  title: {
+    ...text.h2Black,
+  },
+  timeLeftStyle: {
+    ...text.h1Black,
+    ...layout.marginTopS,
+  },
+  explanationBtnTextStyle: {
+    ...text.h2Black,
+    color: colors.mainBlue,
+    fontWeight: 'normal',
+  },
+  explanationBtn: {
+    ...layout.flexRow,
+    ...layout.marginTopS,
+    alignItems: 'center',
+  },
+});
+
+export default ProposalActivationDate;
