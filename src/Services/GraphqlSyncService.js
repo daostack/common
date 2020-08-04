aea071bce2a5209f6fe23cadde869727387db49b
lpetkov-sw <l.petkov@shopdow.com>
2020-08-04 11:46:16 +0300

    Added block Tx in the updateByProposalId parameters

--- src/Services/GraphqlSyncService.js
+++ src/Services/GraphqlSyncService.js
@@ -23,9 +23,9 @@ export default class GraphqlSyncService {
       return this.serviceInstance;
     };
 
-    async syncProposalById(proposalId) {
+    async syncProposalById(proposalId, txBlockNumber) {
       console.log(`update proposal ${proposalId}`);
-      const options = { params: { proposalId, retries: 4 } };
+      const options = { params: { proposalId, retries: 4, txBlockNumber } };
       await this.axiosClient.get('update-proposal-by-id', options);
       console.log('Proposal updated: '); //, proposalUpdateResponse);
     }
