8cffee5e59b37cee4685a2ec7fb8e900c52e178c
lmcmz <lmcmze@gmail.com>
2020-08-14 20:42:49 +1000

    upgrade ipfs version (#432)

--- src/Config/index.js
+++ src/Config/index.js
@@ -26,7 +26,7 @@ if (Config.ENV === 'production') {
   graphUrl = 'https://api.thegraph.com/subgraphs-daostack/name/daostack';
   graphWS = 'wss://api.thegraph.com/subgraphs-daostack/name/daostack';
   ipfsUrl = 'https://api.thegraph.com/ipfs-daostack/api/v0';
-  ipfsDataVersion = '000002';
+  ipfsDataVersion = '000003';
   networkId = 100;
   web3Provider = 'https://dai.poa.network';
   commonTokenAddress = '0x2ea0be07dfc0357f40884365f2c9cfd2a36d4a6e';
@@ -39,7 +39,7 @@ if (Config.ENV === 'production') {
   graphUrl = 'https://api.thegraph.com/subgraphs-daostack/name/daostack';
   graphWS = 'wss://api.thegraph.com/subgraphs-daostack/name/daostack';
   ipfsUrl = 'https://api.thegraph.com/ipfs-daostack/api/v0';
-  ipfsDataVersion = '000002';
+  ipfsDataVersion = '000003';
   networkId = 42;
   web3Provider = 'https://kovan.infura.io/v3/3c08878d00734c0c98a3e4741d0b4cfc';
   commonTokenAddress = '0xdff3e43710d39d2ba5dda7a8d959ed22cc905b01';
