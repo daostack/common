20a71c5bb03ef7910cb650782fd97b1ca99ffcc3
lmcmz <lmcmze@gmail.com>
2020-08-21 16:06:18 +1000

    Add tag to test

--- src/Screens/UserProfile/UserProfile.js
+++ src/Screens/UserProfile/UserProfile.js
@@ -144,6 +144,7 @@ const UserProfile = ({userStore, navigation, route}) => {
                 <AccordionBtn title="HUD test" onPress={onHUDTestPress} />
               </View>}
               <Text style={styles.version}>Common{isProduction ? '' : '-stg'} v{VersionNumber.appVersion} ({VersionNumber.buildVersion}{codePushVersion ? `-${codePushVersion}` : '' })</Text>
+              <Text style={styles.version}> code push is active 22 </Text>
             </View>
           </ScrollView>
         </SafeAreaView>
