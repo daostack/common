9b5151f9a6089773fdf753b4bd990014dd941b89
lmcmz <lmcmze@gmail.com>
2020-08-26 21:21:35 +1000

    Fix conflict

--- src/Screens/Discussions/DiscussionList.js
+++ src/Screens/Discussions/DiscussionList.js
@@ -1,13 +1,9 @@
 import React, {useEffect, useState, useRef} from 'react';
 import {FlatList} from 'react-native';
 import DiscussionCard from './DiscussionCard';
-import firestore from '@react-native-firebase/firestore';
 import ViewTabNoData from '../../Components/ViewTabNoData';
-<<<<<<< Updated upstream
 import {string, object} from 'prop-types';
-=======
 import { db } from '../../Firebase';
->>>>>>> Stashed changes
 
 const DiscussionList = ({commonId, navigation}) => {
   const [list, setList] = useState([]);
