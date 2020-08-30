53af72cd62b9961882b54f5a9bd357d5ebbc4bc0
watchtower314 <moore3.14159@gmail.com>
2020-08-30 17:24:10 -0400

    tabBar renderLabel color wip

--- src/Components/TabView/TabBarRenderer.js
+++ src/Components/TabView/TabBarRenderer.js
@@ -1,52 +1,44 @@
-import { TabBar} from 'react-native-tab-view';
+import {TabBar} from 'react-native-tab-view';
 import React, {useState} from 'react';
-import {View} from 'react-native';
+import {View, Text} from 'react-native';
 
 import TabBarIndicator from './TabBarIndicator';
 import {layout, colors} from '../../Theme';
 import Icon from '../../Assets/iconfont/Icon';
 
 const TabBarRenderer = (props) => {
-
   const [customNavigationState, setCustomNavigationState] = useState(props.navigationState);
   return (
     <TabBar
       ref={props.originRef}
       {...props}
       navigationState={customNavigationState}
-      onTabPress={(currRoute) => {
-        // if there is a parentRef prop then, that's a copy of the tabBar and it's user for appearing as a sticky header for the tabView
-        if (props.parentRef) {
-          currRoute.preventDefault();
-          const parentProps = props.parentRef?.current?.props;
-          setCustomNavigationState(parentProps.navigationState);
-          props.parentRef.current?.props.jumpTo(currRoute.route.key);
-          //props.parentRef.current?.resetScroll(parentProps.navigationState.index);
-        }
+      onTabLongPress={({route}) => {
+        //const {navigationState} = props;
+        //navigationState.index = route.index;
+        //setCustomNavigationState(navigationState);
+        props.jumpTo(route.index);
       }}
-      renderIndicator={(indicator) => {
-        return (
-          <TabBarIndicator
-            position={props.parentRef?.current?.props.position || indicator.position}
-            navigationState={props.parentRef?.current?.props.navigationState || indicator.navigationState}
-            getTabWidth={indicator.getTabWidth}
-            width={indicator.width}
-            style={indicator.style}
-            layout={indicator.layout}
+      renderIndicator={(indicator) => (
+        <TabBarIndicator
+          position={props.parentRef?.current?.props.position || indicator.position}
+          navigationState={props.parentRef?.current?.props.navigationState || indicator.navigationState}
+          getTabWidth={indicator.getTabWidth}
+          width={indicator.width}
+          style={indicator.style}
+          layout={indicator.layout}
+        />
+      )}
+      //props.navigationState.index === route.index
+      renderLabel={({route, focused}) => {
+        const isFocused = props.parentRef ? (props.navigationState.index === route.index) : focused;
+        return <View style={{...layout.content, padding: 0}}>
+          <Icon
+            name={route.iconSelected && isFocused ? route.iconSelected : route.icon}
+            size={30}
+            color={isFocused ? colors.mainBlue : colors.grey3}
           />
-        );
-      }}
-
-      renderLabel={(label, focused) => {
-        return (
-          <View style={{...layout.content, padding: 0}}>
-            <Icon
-              name={label.route.iconSelected && label.focused ? label.route.iconSelected : label.route.icon}
-              size={30}
-              color={label.focused ? colors.mainBlue : colors.grey3}
-            />
-          </View>
-        );
+        </View>;
       }}
       style={
         {
