435e9e86b0cd78a8cc845395d43eaa0f674c2146
Alexander Ivanov <alexander2001ivanov@gmail.com>
2020-09-08 11:47:45 +0300

    New approach

--- src/Components/FormFields/MultiLinkField.js
+++ src/Components/FormFields/MultiLinkField.js
@@ -1,7 +1,7 @@
 import React, {useState} from 'react';
 import {View, TouchableOpacity, Text, StyleSheet} from 'react-native';
 import TextInputField from './TextInputField';
-import { text, layout, colors, sizeL } from '../../Theme';
+import {text, layout, colors, sizeL} from '../../Theme';
 import Icon from '../../Assets/iconfont/Icon';
 import {string, bool, object, number, shape} from 'prop-types';
 
@@ -16,7 +16,7 @@ const MultiLinkField = (props) => {
     placeholderValueText,
     multiline,
     addMultiFieldBtnName,
-    maxLength
+    maxLength,
   } = props;
 
   const onFieldDeleted = (currIndex, currTitleItemValidation, currItemValidation) => {
@@ -28,8 +28,11 @@ const MultiLinkField = (props) => {
     setDeletedFields([ ...deletedFields, currIndex ]);
   };
 
-  const onChangeText = (value, currTitleItemValidation) => {
+  const revalidateAddBtn = () => {
+    setAddButton(canAddMoreLinks());
+  };
 
+  const onChangeText = (value, currTitleItemValidation) => {
     if (value.length > 0) {
       canAddMore();
       validation.formStore.updateFieldValidationRule(currTitleItemValidation.name, currTitleItemValidation.validateRule + '|required');
@@ -156,24 +159,24 @@ const styles = StyleSheet.create({
     height: 30,
     backgroundColor: `${colors.black}10`,
     justifyContent: 'center',
-    alignItems: 'center'
+    alignItems: 'center',
   },
   removeBtn: {
     position: 'absolute',
     top: 0,
     bottom: 0,
     right: 0,
-    justifyContent: 'center'
+    justifyContent: 'center',
   },
   containerRow: {
     flexDirection: 'row',
     alignSelf: 'stretch',
-    marginTop: 80
+    marginTop: 80,
   },
   emailContainer: {
     ...layout.content,
     ...layout.marginBottomXL,
-    marginTop: 0
+    marginTop: 0,
   },
   containerRow: {
     flexDirection: 'row',
@@ -189,8 +192,8 @@ const styles = StyleSheet.create({
     ...text.h3Black,
     color: colors.mainBlue,
     textAlign: 'left',
-    fontSize: 16
-  }
+    fontSize: 16,
+  },
 });
 
 export default MultiLinkField;
