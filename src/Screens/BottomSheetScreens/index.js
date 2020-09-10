75bf98e467e1bbf47a850e9c73e36eebc569e488
lpetkov-sw <lyubomir.petkov@limechain.tech>
2020-09-10 16:45:45 +0300

    Added missing import

--- src/Screens/BottomSheetScreens/index.js
+++ src/Screens/BottomSheetScreens/index.js
@@ -6,5 +6,6 @@ export SortProposals from './SortProposals';
 export UnsavedChanges from './UnsavedChanges';
 export UserProfileSheetScreen from './UserProfileSheetScreen';
 export TransactionError from './TransactionError';
+export PublishCommon from './PublishCommon';
 
 export {default as SafetyPeriodAbout} from './SafetyPeriodAbout';
