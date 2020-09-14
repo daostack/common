a612d1ee8c310570757a777e54556fffff64da19
watchtower <30501647+watchtower314@users.noreply.github.com>
2020-09-14 13:03:09 -0400

    logger fix - display readable objects (#522)

--- src/Services/Logger.js
+++ src/Services/Logger.js
@@ -1,28 +1,12 @@
 class logger {
 
-	log = (...message) => __DEV__ && console.log(this.inputToString(message));
+  log = (...message) => __DEV__ && console.log(...message);
 
-	error = (...error) => __DEV__ && console.error(this.inputToString(error));
+  error = (...error) => __DEV__ && console.error(...error);
 
-	info = (...info) => __DEV__ && console.info(this.inputToString(info));
+  info = (...info) => __DEV__ && console.info(...info);
 
-	warn = (...warning) => __DEV__ && console.warn(this.inputToString(warning));
-
-  inputToString = (input) => {
-    try {
-      return input.length === 1
-        ? input[0]
-        : input.map((arg) => (
-          arg = typeof arg === 'object'
-            ? JSON.stringify(arg)
-            : arg
-        )).join(' ');
-    } catch (err) {
-      console.log('CATCH ERROR IN LOGGER');
-      return input;
-    }
-  }
-
+  warn = (...warning) => __DEV__ && console.warn(...warning);
 }
 
 export default new logger();
