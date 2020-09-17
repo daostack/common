a1af5bfb13ffa34dfcf8776ade6084a96e9d0078
Alexander Ivanov <alexander2001ivanov@gmail.com>
2020-09-17 11:05:05 +0300

    Fix text being under the keyboard

--- src/Screens/Proposals/ProposalScreen.js
+++ src/Screens/Proposals/ProposalScreen.js
@@ -150,7 +150,6 @@ const ProposalScreen = ({
     {key: 'discussions', icon: 'discussion', iconSelected: 'discussion-selected'},
   ]);
 
-  const [ inputHeight, setInputHeight ] = useState(60);
   const [ inputText, setInputText ] = useState(null);
 
   const inputRef = useRef();
@@ -216,15 +215,11 @@ const ProposalScreen = ({
               <TextInput
                 ref={inputRef}
                 editable={true}
-                multiline={true}
                 fontSize={15}
                 onChangeText={(currText) => setInputText(currText)}
-                onContentSizeChange={(e) => {
-                  setInputHeight(e.nativeEvent.contentSize.height);
-                }}
                 style={{
                   flex: 1,
-                  height: inputHeight,
+                  height: 18,
                   marginHorizontal: 10,
                 }}
               />
