de8b2160a6e8fab261e3f5e5492b8836bcefe580
lpetkov-sw <lyubomir.petkov@limechain.tech>
2020-11-17 15:53:35 +0200

    Fixed eslint warning

--- src/Screens/Proposals/FundingProposal.js
+++ src/Screens/Proposals/FundingProposal.js
@@ -15,7 +15,7 @@ import {CommonActions} from '@react-navigation/native';
 import Toast from '~/Util/Toast';
 import font from '~/Theme/font';
 import logger from '~/Services/Logger';
-import {string, object, shape, func} from 'prop-types';
+import {string, object, shape} from 'prop-types';
 import FundingRequestFormStore from '~/FormStores/FundingRequestFormStore';
 
 import ProposalService from '~/Services/ProposalService';
