004bdc12f3263620c8769e15492939192bf9861a
Ilya <at.ilya@yahoo.com>
2020-11-17 16:29:10 +0100

    Cm 1256 close button (#675)
    * Add close button
    * Confirm dialog before closing common

--- src/Screens/Commons/CreateCommon/CreateStepNavigation.js
+++ src/Screens/Commons/CreateCommon/CreateStepNavigation.js
@@ -2,9 +2,11 @@ import React from 'react';
 import {TouchableOpacity, StyleSheet} from 'react-native';
 import NavigationBar from 'react-native-navbar';
 import Icon from '~/Assets/iconfont/Icon';
-import {string, object} from 'prop-types';
+import {observer, inject} from 'mobx-react';
+import {string, object, shape, func} from 'prop-types';
+import {BOTTOM_SHEET_TEMPLATES} from '~/Stores/BottomSheetStore';
 
-const CreateStepNavigation = ({title, navigation}) => (
+const CreateStepNavigation = ({title, bottomSheetStore, navigation}) => (
   <NavigationBar
     statusBar={{hidden: true}}
     title={{
@@ -17,12 +19,37 @@ const CreateStepNavigation = ({title, navigation}) => (
         <Icon name="left-arrow" size={28} style={styles.icon} color="black" />
       </TouchableOpacity>
     }
+    rightButton={
+      <TouchableOpacity
+        style={{justifyContent: 'center'}}
+        onPress={() => {
+          bottomSheetStore.showBottomSheet(BOTTOM_SHEET_TEMPLATES.UNSAVED_CHANGES, {
+            navigation: navigation,
+            onContinueEditing: () => bottomSheetStore.hideBottomSheet(),
+            onLeaveWithoutSaving: () => {
+              bottomSheetStore.hideBottomSheet();
+              navigation.popToTop();
+            },
+          });
+        }}>
+        <Icon
+          name="close"
+          size={18}
+          style={{marginRight: 20}}
+          color="black"
+        />
+      </TouchableOpacity>
+    }
   />
 );
 
 CreateStepNavigation.propTypes = {
   title: string,
   navigation: object,
+  bottomSheetStore: shape({
+    showBottomSheet: func,
+    hideBottomSheet: func,
+  }),
 };
 
 const styles = StyleSheet.create({
@@ -32,4 +59,6 @@ const styles = StyleSheet.create({
   icon: {marginLeft: 20},
 });
 
-export default CreateStepNavigation;
+export default inject(
+  'bottomSheetStore',
+)(observer(CreateStepNavigation));
