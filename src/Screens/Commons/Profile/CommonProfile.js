20447fe23a2afdc491e1e9f069bb5450b010a978
lpetkov-sw <lyubomir.petkov@limechain.tech>
2020-11-20 13:29:07 +0200

    CM-1302 Fixed rules step appearing in create common flow.

--- src/Screens/Commons/Profile/CommonProfile.js
+++ src/Screens/Commons/Profile/CommonProfile.js
@@ -368,7 +368,7 @@ const CommonProfile = ({
   */
 
   const calcShouldSkipRules = () => {
-    const rules = currCommon.metadata?.rules;
+    const rules = currCommon?.rules;
     if (rules?.length > 0) {
       // NOTE: value of multiple fields was stored in url prop before
       return !rules.some((rule) => rule?.title && (rule?.value || rule.url));
