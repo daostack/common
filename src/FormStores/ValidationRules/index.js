72b8603ce0d93588a7481683e91979380a697c25
watchtower <30501647+watchtower314@users.noreply.github.com>
2020-12-07 09:56:46 +0200

    CM 1262 exp date validation (#724)

--- src/FormStores/ValidationRules/index.js
+++ src/FormStores/ValidationRules/index.js
@@ -73,13 +73,13 @@ export const validDateFormat = {
     if (month.length !== 2 || year.length !== 2) {
       return false;
     }
-    return !isNaN(month) && !isNaN(year);
+    return !isNaN(month) && !isNaN(year) && (month > 0 && month <= 12);
   },
-  errorMessage: 'The date format should be "MM/YY".',
+  errorMessage: 'Invalid expiration date.',
 };
 
 export const futureDate = {
   ruleName: VALIDATION_RULES.CARD_EXP_DATE,
-  validateFunc: (value, requirement, attribute) => isExpired(value), //!moment(fixDate(value)).isBefore(moment().format('YY/MM')),
+  validateFunc: (value, requirement, attribute) => isExpired(value),
   errorMessage: 'Expiration date has passed.',
 };
