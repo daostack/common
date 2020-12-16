2e5fa953b0592978918800441fea98de2ac486b3
lpetkov-sw <lyubomir.petkov@limechain.tech>
2020-12-16 09:14:29 +0200

    revert helper.tsx

---
+++ src/Assets/iconfont/helper.tsx
@@ -0,0 +1,5 @@
+import _ from 'lodash'
+
+export function getIconColor(color: string | undefined, num: number, defaultColor: string): string {
+    return _.isEmpty(color) ? defaultColor : color!
+}
