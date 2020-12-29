fc61756ca44d0b16107be4ca7660946093cdc201
lpetkov-sw <lyubomir.petkov@limechain.tech>
2020-12-29 16:53:11 +0200

    null safe for formatting utils

--- src/Util/FormatUtil.ts
+++ src/Util/FormatUtil.ts
@@ -1,25 +1,25 @@
 
-export const unFormatNumber = (currValue: string) => currValue.replace(',', '');
+export const unFormatNumber = (number: string) => number ? number.replace(',', '') : '';
 
-export const formatNumber = (currValue: string | number) => {
+export const formatNumber = (number: string | number) => {
 
   /* The next line is making the whole formatting right, but it's not working for Android.
      * In order to make it work on Android we have to change android/build.gradle and add  `def jscFlavor = 'org.webkit:android-jsc-intl:+'`
      * Let's do it on the next build requred changes. (More info: https://github.com/lingui/js-lingui/issues/442)
     */
 
-  // new Intl.NumberFormat('en-US').format(currValue);
+  // new Intl.NumberFormat('en-US').format(number);
 
-  if (!currValue) {
+  if (!number) {
     return '';
   }
 
   let dec = '';
 
-  if (currValue?.toString().includes('.')) {
-    [currValue, dec] = currValue?.toString().split('.');
+  if (number?.toString().includes('.')) {
+    [number, dec] = number?.toString().split('.');
     dec = `.${dec}`;
   }
-  currValue = `${parseFloat(currValue?.toString()).toLocaleString('en-US')}${dec}`;
-  return currValue;
+  number = `${parseFloat(number?.toString()).toLocaleString('en-US')}${dec}`;
+  return number;
 };
