3da5161e7f655feef3730cd04f9406964ab60269
lpetkov-sw <lyubomir.petkov@limechain.tech>
2021-01-11 17:21:35 +0200

    Removed Toast from data layer

--- src/Services/ListServices/UserListService.ts
+++ src/Services/ListServices/UserListService.ts
@@ -1,47 +1,37 @@
 import {UserCollection} from '~/Firebase/Databasee/Collections/UsersCollection';
-import Toast from '~/Util/Toast';
 
 export const subscribeToCommonlUsers = (commonId, callback) =>
-  UserCollection.where('').onSnapshot(
-    (snapshot) => {
-      let userList = [];
+  UserCollection.where('').onSnapshot((snapshot) => {
+    let userList = [];
 
-      // TODO: Make better handling of changes with docChanges()
-      if (!snapshot?.empty || !snapshot) {
-        userList = snapshot.docs.map((doc) => doc.data());
-      }
+    // TODO: Make better handling of changes with docChanges()
+    if (!snapshot?.empty || !snapshot) {
+      userList = snapshot.docs.map((doc) => doc.data());
+    }
 
-      callback(userList);
-    },
-    (error) => Toast.error(error),
-  );
+    callback(userList);
+  });
 
 export const subscribeToAllUsers = (callback) =>
-  UserCollection.onSnapshot(
-    (snapshot) => {
-      let userList = [];
+  UserCollection.onSnapshot((snapshot) => {
+    let userList = [];
 
-      // TODO: Make better handling of changes with docChanges()
-      if (!snapshot?.empty || !snapshot) {
-        userList = snapshot.docs.map((doc) => doc.data());
-      }
+    // TODO: Make better handling of changes with docChanges()
+    if (!snapshot?.empty || !snapshot) {
+      userList = snapshot.docs.map((doc) => doc.data());
+    }
 
-      callback(userList);
-    },
-    (error) => Toast.error(error),
-  );
+    callback(userList);
+  });
 
 export const subscribeToUser = (uid, callback) =>
-  UserCollection.doc(uid).onSnapshot(
-    (snapshot) => {
-      let user = null;
+  UserCollection.doc(uid).onSnapshot((snapshot) => {
+    let user = null;
 
-      // TODO: Make better handling of changes with docChanges()
-      if (!snapshot?.empty || !snapshot) {
-        user = snapshot.docs.map((doc) => doc.data());
-      }
+    // TODO: Make better handling of changes with docChanges()
+    if (!snapshot?.empty || !snapshot) {
+      user = snapshot.docs.map((doc) => doc.data());
+    }
 
-      callback(user);
-    },
-    (error) => Toast.error(error),
-  );
+    callback(user);
+  });
