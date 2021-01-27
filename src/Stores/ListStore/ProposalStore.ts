11275210b02e97bc636179fae6f0a6fb5f41bb41
lpetkov-sw <lyubomir.petkov@limechain.tech>
2021-01-27 10:31:43 +0200

    Commented setUpdates function in order to avoid crashing.

--- src/Stores/ListStore/ProposalStore.ts
+++ src/Stores/ListStore/ProposalStore.ts
@@ -118,12 +118,14 @@ export default class ProposalStore extends ListStore<Proposal> {
       .forEach((updatedProposalDoc: IFirebaseDocChange<IProposalEntity>) => {
         const updatedProposal = updatedProposalDoc.doc.data();
 
-        let proposal = this.getDataById(updatedProposal.id);
-        if (proposal) {
-          proposal.setUpdates(updatedProposal);
-        } else {
-          this.setData(updatedProposal.id, new Proposal(updatedProposal));
-        }
+        // let proposal = this.getProposalById(
+        //   updatedProposal.id,
+        // );
+        // if (proposal) {
+        //   proposal.setUpdates(updatedProposal);
+        // }
+
+        this.setData(updatedProposal.id, new Proposal(updatedProposal));
       });
 
     runInAction(() => {
