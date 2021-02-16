60db578f9ab0591ae04f543819887ff3801dca65
IvanSantiagoUk <ivansantiagouk@gmail.com>
2021-02-16 13:03:11 +0100

    CM-1186 Discussion - join the discussion - The message when user is not a member hides the text (#925)

--- src/Screens/Discussions/Discussions.js
+++ src/Screens/Discussions/Discussions.js
@@ -217,7 +217,7 @@ const Discussions = ({
           setInputText('');
           await DiscussionService.getInstance().updateDiscussionLastMessage(
             discussionId,
-            currentUser.uid
+            currentUser.uid,
           );
         })
         .catch((error) => {
@@ -490,7 +490,7 @@ const Discussions = ({
               </TouchableOpacity>
             </View>
           ) : (
-            <Text style={{...styles.joinCommonText}}>
+            <Text style={styles.joinCommonText}>
               {'Only members can send messages'}
             </Text>
           )}
@@ -707,6 +707,8 @@ const styles = StyleSheet.create({
   },
   joinCommonText: {
     ...text.textFieldplaceholder,
+    width,
+    textAlign: 'center',
     color: colors.greySubtitle,
     paddingTop: sizeS,
     paddingBottom: sizeXL,
