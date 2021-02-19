98fb27f5a607fb161b9444134086b86bc32dba0f
Pavel Meyer <meyerpavel1207@gmail.com>
2021-02-19 23:53:03 +0300

    Added isFullScreen mode for loader

--- src/Components/Loader.js
+++ src/Components/Loader.js
@@ -3,7 +3,7 @@ import React from 'react';
 import {layout, colors, sizeXXL} from '~/Theme';
 import {string, bool} from 'prop-types';
 
-const Loader = ({color, isBigger}) => {
+const Loader = ({color, isBigger, isFullScreen = false}) => {
   let loaderStyle = isBigger
     ? {
         ...styles.loader,
@@ -12,11 +12,15 @@ const Loader = ({color, isBigger}) => {
     : styles.loader;
 
   return (
-    <View styl={styles.loaderContainer}>
+    <View
+      style={[
+        styles.loaderContainer,
+        isFullScreen ? styles.loaderFullScreenContainer : {},
+      ]}>
       <ActivityIndicator
         size="large"
         color={color || colors.mainBlue}
-        style={loaderStyle}
+        style={[loaderStyle, isFullScreen ? styles.loaderFullScreen : {}]}
       />
     </View>
   );
@@ -25,6 +29,7 @@ const Loader = ({color, isBigger}) => {
 Loader.propTypes = {
   color: string,
   isBigger: bool,
+  isFullScreen: bool,
 };
 
 const styles = StyleSheet.create({
@@ -32,6 +37,16 @@ const styles = StyleSheet.create({
     ...layout.content,
     ...layout.flexStart,
     alignSelf: 'stretch',
+  },
+  loaderFullScreenContainer: {
+    position: 'absolute',
+    backgroundColor: '#fff',
+    height: '100%',
+    width: '100%',
+  },
+  loaderFullScreen: {
+    display: 'flex',
+    flex: 1,
   },
   loader: {
     marginTop: sizeXXL,
