f71fa62a9dfe425bead5cd7670f6c4d426b620a5
lpetkov-sw <lyubomir.petkov@limechain.tech>
2021-03-10 16:53:03 +0200

    Fixes

--- src/Stores/DataStores/NotificationStore.ts
+++ src/Stores/DataStores/NotificationStore.ts
@@ -18,10 +18,15 @@ export default class NotificationStore extends BaseStore<
     this.getDataById(id);
 
   getLoggedUserNotifications = (): Array<Notification> | undefined =>
-    this.getDataArray?.filter(
-      (notification: Notification) =>
-        notification.notificationItemData?.missingData === false,
-    );
+    this.getDataArray
+      ?.filter(
+        (notification: Notification) =>
+          notification.notificationItemData?.missingData === false,
+      )
+      .sort(
+        (notification: Notification, prevNotification: Notification) =>
+          prevNotification.createdAt?.seconds - notification.createdAt?.seconds,
+      );
   //Actions
   subscribeToUserNotifications = (userId: string): FirestoreUnsubscribeFn =>
     subscribeToUserNotifications(userId, this.updateStoreData);
