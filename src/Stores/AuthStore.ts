c61a7771e00fc227022601f60feb466eafe87ff4
Ivan Santiago Prieto <ivansantiagouk@gmail.com>
2021-03-17 10:25:30 +0100

    fixing asyncstorage import

--- src/Stores/AuthStore.ts
+++ src/Stores/AuthStore.ts
@@ -1,4 +1,4 @@
-import {AsyncStorage} from '@react-native-community/async-storage';
+import AsyncStorage from '@react-native-community/async-storage';
 import {observable, action} from 'mobx';
 import {isDaoMemberByUserId} from '~/Util';
 import logger from '~/Services/Logger';
