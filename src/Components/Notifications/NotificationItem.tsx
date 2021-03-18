505e896969896949fd907319f4fc3bee3e39d82f
lpetkov-sw <lyubomir.petkov@limechain.tech>
2021-03-18 14:57:29 +0200

    Disabled feature for showing seen & clicked notification items (#984)

--- src/Components/Notifications/NotificationItem.tsx
+++ src/Components/Notifications/NotificationItem.tsx
@@ -48,14 +48,13 @@ const props = {
 const NotificationItem: React.FC<InferProps<typeof props>> = ({
   item,
   navigation,
-  notificationStore,
 }) => {
   const navigateToDetail = () => {
     let navigate;
 
-    notificationStore.setNotificationItemState(item.id, {
-      opened: true,
-    });
+    // notificationStore.setNotificationItemState(item.id, {
+    //   opened: true,
+    // });
 
     if (item.notificationItemData.proposal) {
       navigation.navigate(NAVIGATION_SCREENS.PROPOSAL_SCREEN, {
@@ -100,9 +99,10 @@ const NotificationItem: React.FC<InferProps<typeof props>> = ({
         style={[
           styles.messageCardContainer,
           {
-            backgroundColor: item.notificationItemState.opened
-              ? colors.white
-              : colors.paleNotificationblue,
+            // backgroundColor: item.notificationItemState.opened
+            //   ? colors.white
+            //   : colors.paleNotificationblue,
+            backgroundColor: colors.white,
           },
         ]}>
         <View
@@ -113,9 +113,9 @@ const NotificationItem: React.FC<InferProps<typeof props>> = ({
               uri: item.notificationItemData.ownerAvatar,
             }}
           />
-          {!item.notificationItemState.seen && (
+          {/* {!item.notificationItemState.seen && (
             <View style={styles.notReadDot} />
-          )}
+          )} */}
         </View>
         <View>
           <View style={styles.headerContainer}>
