5655d5656533197bbe2b39f93876cbce9f280088
watchtower <30501647+watchtower314@users.noreply.github.com>
2021-04-01 06:16:33 -0400

    fix scroll for post header (#1012)

--- src/Screens/Discussions/Discussions.js
+++ src/Screens/Discussions/Discussions.js
@@ -240,14 +240,20 @@ const Discussions = ({
         //   </TouchableOpacity>
         // }
       />
-      <View style={{overflow: 'hidden', paddingBottom: 5}}>
-        <View style={styles.headerContainer}>
+      <View
+        style={{
+          overflow: 'hidden',
+          paddingBottom: 5,
+          height: dataState.isExpanded ? '60%' : '5%',
+        }}>
+        <View style={{...styles.headerContainer, height: '100%'}}>
           {dataState.isExpanded ? (
             <View
               style={{
                 paddingTop: 20,
                 paddingHorizontal: 20,
                 shadowColor: 'rgba(0, 0, 0, 0.12)',
+                height: '100%',
               }}>
               <ScrollView>
                 <View
