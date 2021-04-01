e241679097ff77e1f0952902aa19900ce0384f75
lpetkov-sw <lyubomir.petkov@limechain.tech>
2021-04-01 20:00:37 +0300

    disabled user info checker (#1021)

--- App.js
+++ App.js
@@ -68,7 +68,6 @@ import logger from './src/Services/Logger';
 import {fontSize} from './src/Theme/font';
 import Loader from '~/Components/Loader';
 import crashlytics from '@react-native-firebase/crashlytics';
-import UserInfoChecker from '~/Screens/UserProfile/UserInfoChecker';
 
 const Stack = createStackNavigator();
 I18nManager.allowRTL(false);
@@ -613,7 +612,7 @@ const App = ({rootStore, navigation}) => {
           navigation={navigationRef}
         />
       )}
-      <UserInfoChecker navigation={navigationRef} />
+      {/* <UserInfoChecker navigation={navigationRef} /> */}
       {appLoaderStore.isLoading && (
         <Loader isBigger isFullScreen navigation={navigationRef} />
       )}
