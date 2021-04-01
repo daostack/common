e24cfcaf598a06726652c5bc2a7a4a3e3e9f7bff
lpetkov-sw <lyubomir.petkov@limechain.tech>
2021-04-01 17:33:21 +0300

    Reverted change about adding welcome notification

--- src/Stores/AuthStore.ts
+++ src/Stores/AuthStore.ts
@@ -103,8 +103,6 @@ class AuthStore {
     this.userInfo = newUserInfo;
     if (isUserChanged) {
       this.signedInUser = newUserInfo?.uid;
-    }
-    if (this.userInfo) {
       this.rootStore.notificationStore.addWelcomeNotification();
     }
   };
