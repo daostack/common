0b3809ecaf90549be3029511e058e5fef832acb9
lpetkov-sw <lyubomir.petkov@limechain.tech>
2021-04-01 17:31:18 +0300

    Removed log

--- src/Services/AuthService.js
+++ src/Services/AuthService.js
@@ -93,7 +93,6 @@ export default class AuthService {
       await this.googleSignOut();
       await auth().signOut();
     } catch (error) {
-      console.log('ERRRRRROR -> ', error);
       const {accessToken} = await GoogleSignin.getTokens();
       await GoogleSignin.clearCachedAccessToken(accessToken);
       return error;
