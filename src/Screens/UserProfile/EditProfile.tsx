42449d8b28e085df209670161905ae6d428783b0
Pavel Meyer <meyerpavel1207@gmail.com>
2021-04-05 16:56:13 +0300

    Fix latin only

--- src/Screens/UserProfile/EditProfile.tsx
+++ src/Screens/UserProfile/EditProfile.tsx
@@ -25,10 +25,17 @@ import logger from '~/Services/Logger';
 import EditProfileFormStore from '~/FormStores/EditProfileFormStore';
 import {AppRootStore} from '~/Types/store';
 import {WithNavigation} from '~/Types/navigation';
+import {latinOnly} from '~/Util/constants/validation';
 
 const validationSchema = object({
-  firstName: string().required().label('The first name'),
-  lastName: string().required().label('The last name'),
+  firstName: string()
+    .matches(latinOnly, 'The first name should use latin characters only')
+    .required()
+    .label('The first name'),
+  lastName: string()
+    .matches(latinOnly, 'The first name should use latin characters only')
+    .required()
+    .label('The last name'),
   photoURL: string(),
   intro: string().label('The intro'),
 });
