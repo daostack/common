35b00fe732c678f2f6961a6979fa668827293260
Ivan Santiago Prieto <ivansantiagouk@gmail.com>
2021-04-08 13:54:43 +0200

    adding margin bottom and dot

--- src/Screens/Commons/RequestToJoin/Steps/ContributionStep.js
+++ src/Screens/Commons/RequestToJoin/Steps/ContributionStep.js
@@ -204,7 +204,7 @@ const ContributionStep = ({
 
         {isMonthly && (
           <Text style={styles.monthlyBottomMessage}>
-            You can cancel the recurring payment at any time
+            You can cancel the recurring payment at any time.
           </Text>
         )}
 
@@ -244,6 +244,7 @@ const styles = StyleSheet.create({
     ...text.regularText,
     textAlign: 'center',
     color: colors.slate,
+    marginBottom: 10,
   },
 });
 
