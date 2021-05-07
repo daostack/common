87d2f61bba16bb6f61cebb39dea4d8ff8894b153
watchtower <30501647+watchtower314@users.noreply.github.com>
2021-05-07 16:19:41 +0300

    CM - 169 moderation improvements (#1044)

--- src/Screens/Proposals/ProposalScreen.js
+++ src/Screens/Proposals/ProposalScreen.js
@@ -775,10 +775,7 @@ const ProposalScreen = ({
                         isScreenHeader={true}
                         state={proposalInfo?.state}
                         paymentStatus={proposalInfo?.paymentState}
-                        closingAt={
-                          proposalInfo?.createdAt.seconds +
-                          proposalInfo?.countdownPeriod
-                        }
+                        closingAt={proposalInfo?.countdown}
                         onPress={() => openDebtInsufficientModal()}
                         hasPermission={hasPermission}
                         viewerPermission={viewerPermission}
@@ -812,10 +809,7 @@ const ProposalScreen = ({
                         isScreenHeader={true}
                         state={proposalInfo?.state}
                         paymentStatus={proposalInfo?.paymentState}
-                        closingAt={
-                          proposalInfo?.createdAt.seconds +
-                          proposalInfo?.countdownPeriod
-                        }
+                        closingAt={proposalInfo?.countdown}
                         hasPermission={hasPermission}
                         authInfo={authStore.userInfo}
                         viewerPermission={viewerPermission}
