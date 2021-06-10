3211804b5c7a73483c23dc6f840d50329e2f221d
Batraz Jioty <batrajik9@gmail.com>
2021-06-10 09:59:31 +0300

    CM-498: Replace the order in membership requests (#1090)
    Co-authored-by: batraz <baticpro@gmail.com>

--- src/Screens/Commons/Profile/CommonProfile.js
+++ src/Screens/Commons/Profile/CommonProfile.js
@@ -51,7 +51,7 @@ import * as ModerationForm from '~/Components/Forms/ModerationForm';
 import {reporterName, timeReported} from '~/Components/Moderation/Reported';
 import ModerationActionSuccessModal from '~/Components/Moderation/ModerationActionSuccessModal';
 import ModerationModal from '~/Components/Moderation/ModerationModal';
-import Toast from '~/Util/Toast.js';
+import Toast from '~/Util/Toast';
 import {TITLES, ACTIONS} from '~/Components/Moderation/constants';
 
 import {
@@ -423,7 +423,7 @@ const CommonProfile = ({navigation, route: {params}, rootStore}) => {
     </View>
   );
 
-  const openCommonMembers = (e) => {
+  const openCommonMembers = () => {
     navigation.navigate('CommonMembers', {
       commonId: currCommon.id,
       screenTitle: currCommon.name,
@@ -436,7 +436,7 @@ const CommonProfile = ({navigation, route: {params}, rootStore}) => {
     });
   };
 
-  const shareCommon = (event) => {
+  const shareCommon = () => {
     const options = {
       url: `https://app.common.io/common/${currCommon.id}`,
       title: "Let's make it happen",
@@ -550,27 +550,15 @@ const CommonProfile = ({navigation, route: {params}, rootStore}) => {
   };
   */
 
-  const calcShouldSkipRules = () => {
-    const rules = currCommon?.rules;
-    if (rules?.length > 0) {
-      // NOTE: value of multiple fields was stored in url prop before
-      return !rules.some((rule) => rule?.title && (rule?.value || rule.url));
-    } else {
-      return true;
-    }
-  };
-
-  const requestToJoin = (event) => {
+  const requestToJoin = () => {
     if (authStore.userInfo) {
-      const shouldSkipRules = calcShouldSkipRules();
-
       const introduceYourselfFormStore = new IntroduceYourselfFormStore();
       const paymentFormStore = new PaymentFormStore();
       const personalContributionFormStore = new PersonalContributionFormStore();
       const billingDetailsFormStore = new BillingDetailsFormStore();
 
       const navigate = CommonActions.navigate({
-        name: shouldSkipRules ? 'IntroductionStep' : 'RulesStep',
+        name: 'IntroductionStep', // #498 we always go to Introduction first
         params: {
           formStores: {
             paymentFormStore,
@@ -580,7 +568,7 @@ const CommonProfile = ({navigation, route: {params}, rootStore}) => {
           },
           currCommon: currCommon,
           currDaoId: currCommon.id,
-          skipFirstStep: shouldSkipRules,
+          skipFirstStep: false,
           refreshFeed,
         },
       });
