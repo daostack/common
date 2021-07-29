77df2f1cdd1a01c918aba42d38f563c0ba6ee01c
watchtower314 <moore3.14159@gmail.com>
2021-07-29 14:46:23 +0300

    mastercard hotfix

--- src/Screens/Commons/RequestToJoin/Steps/IntroductionStep.js
+++ src/Screens/Commons/RequestToJoin/Steps/IntroductionStep.js
@@ -10,6 +10,7 @@ import RequestStepHeaderTitle from '../RequestStepHeaderTitle';
 import MembershipRequest from '../MembershipRequest';
 import {string, object, bool, shape, func} from 'prop-types';
 import StepDotLayout from '~/Components/Layouts/StepDotLayout';
+import {PurpleBoxMessage} from '~/Components/PurpleBoxMessage';
 
 const IntroductionStep = ({
   navigation,
@@ -62,6 +63,9 @@ const IntroductionStep = ({
           title="Introduce Yourself"
           subtitle="Let the Common members learn more about you and how you relate to the cause."
         />
+
+        <PurpleBoxMessage message="Please note: cuttently, credit cards issued by Mastercard are not supported." />
+
         <View
           style={{
             backgroundColor: colors.grey4,
