28e0783a9e4235c2a592d2f4ddd329ff5082cbc6
watchtower <30501647+watchtower314@users.noreply.github.com>
2021-12-16 16:05:26 +0200

    disable joining common (#1163)

--- src/Screens/Commons/Profile/CommonProfile.js
+++ src/Screens/Commons/Profile/CommonProfile.js
@@ -954,14 +954,14 @@ const CommonProfile = ({navigation, route: {params}, rootStore}) => {
 
             {renderMembersRow()}
 
-            {!isMember && showReqToJoin && (
+            {/*!isMember && showReqToJoin && (
               <View
                 style={styles.upperActionButtonContainer}
                 ref={upperRequestToJoinBtnRef}
                 collapsable={false}>
                 {renderRequestToJoinBtn()}
               </View>
-            )}
+            )*/}
 
             {renderAgendaForNonMembers()}
 
