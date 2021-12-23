8e5d0c76ba33faef1e8f4cefbe4c63be830d545a
watchtower <30501647+watchtower314@users.noreply.github.com>
2021-12-23 14:10:09 +0200

    Cm test payme webview (#1164)
    * Added onNavigationStateChange handler to webview
    
    * create request to join
    
    Co-authored-by: Pavel Meyer <p.meyer@kernelics.com>

--- src/Screens/Commons/RequestToJoin/Steps/PaymentDetailsStep.js
+++ src/Screens/Commons/RequestToJoin/Steps/PaymentDetailsStep.js
@@ -1,27 +1,38 @@
-import React, {useEffect} from 'react';
+import React, {useState} from 'react';
 import {View, Dimensions} from 'react-native';
-import {colors, text} from '~/Theme';
+//import {colors, text} from '~/Theme';
 import {inject} from 'mobx-react';
-import RequestStepActionButton from '../../RequestStepActionButton';
+import {CommonActions} from '@react-navigation/native';
+//import RequestStepActionButton from '../../RequestStepActionButton';
 import {string, func, bool, object, shape} from 'prop-types';
 import MembershipRequest from '../MembershipRequest';
 import StepDotLayout from '~/Components/Layouts/StepDotLayout';
 import {BOTTOM_SHEET_TEMPLATES} from '~/Screens/BottomSheetScreens';
 import {rootStorePropTypes} from '~/Types/propTypes';
 import {WebView} from 'react-native-webview';
+import ProposalService from '~/Services/ProposalService';
 import {escapeUrl} from '~/Util';
 import Toast from '~/Util/Toast';
+import {showErrorPopUp} from '~/Util';
 const {height} = Dimensions.get('window');
 
 const PaymentDetailsStep = ({
   navigation,
   route: {
-    params: {formStores, skipFirstStep, currCommon, currDaoId, refreshFeed, iFrameLink},
+    params: {
+      formStores,
+      skipFirstStep,
+      currCommon,
+      currDaoId,
+      refreshFeed,
+      iFrameLink,
+    },
   },
   rootStore,
 }) => {
   const userInfo = rootStore.authStore.userInfo;
   const bottomSheetStore = rootStore.uiStore.bottomSheetStore;
+  const [respLink, setRespLink] = useState('');
 
   const isMonthly = currCommon.metadata.contributionType === 'monthly';
 
@@ -32,81 +43,86 @@ const PaymentDetailsStep = ({
   const billingDetailsFormStore = formStores.billingDetailsFormStore;
 
   const push = async () => {
-    if (!billingDetailsFormStore.isFormValid()) {
-      navigation.pop();
-    } else if (paymentFormStore.isFormValid()) {
-      try {
-        const formData = {
-          ...introduceYourselfFormStore.getFormFieldsJson(),
-          ...personalContributionFormStore.getFormFieldsJson(),
-          ...paymentFormStore.getFormFieldsJson(),
-          ...billingDetailsFormStore.getFormFieldsJson(),
-        };
+    try {
+      const formData = {
+        ...introduceYourselfFormStore.getFormFieldsJson(),
+        ...personalContributionFormStore.getFormFieldsJson(),
+        ...billingDetailsFormStore.getFormFieldsJson(),
+      };
 
-        const data = {
-          description: formData.intro,
-          funding: formData.amount * 100,
-          commonId: currDaoId,
-        };
+      const data = {
+        description: formData.intro,
+        funding: formData.amount * 100,
+        commonId: currDaoId,
+      };
 
-        if (formData.links) {
-          data.links = escapeUrl(formData.links);
-        }
+      if (formData.links) {
+        data.links = escapeUrl(formData.links);
+      }
 
-        //const resp = await BillingDetailsService.add(billingDetailsFormStore.getFormFieldsJson());
-        /*navigation.navigate({
-          name: 'FullScreenCreationLoader',
+      //const resp = await BillingDetailsService.add(billingDetailsFormStore.getFormFieldsJson());
+      /*navigation.navigate({
+        name: 'FullScreenCreationLoader',
+        params: {
+          title: 'Creating your membership request',
+        },
+      });*/
+
+      /*const createdCard = await CirclePayService.createCard({
+        ...formData,
+        links: escapeUrl(formData.links),
+        ...userInfo,
+      });*/
+
+      const createRequestToJoinResponse = await ProposalService.createRequestToJoin(
+        {
+          ...data,
+          cardId: userInfo.uid,
+        },
+      );
+
+      if (createRequestToJoinResponse.status === 200) {
+        const proposalId = createRequestToJoinResponse.data.id;
+
+        //navigation.pop();
+        const navigate = CommonActions.navigate({
+          name: 'CommonProfile',
           params: {
-            title: 'Creating your membership request',
+            showRequestSentModal: true,
+            createdProposalId: proposalId,
           },
         });
 
-        const createdCard = await CirclePayService.createCard({
-          ...formData,
-          links: escapeUrl(formData.links),
-          ...userInfo,
-        });
+        if (typeof refreshFeed === 'function') {
+          refreshFeed();
+        }
 
-        const createRequestToJoinResponse =
-          await ProposalService.createRequestToJoin({
-            ...data,
-            cardId: createdCard.id,
-          });
-
-        if (createRequestToJoinResponse.status === 200) {
-          const proposalId = createRequestToJoinResponse.data.id;
-
-          navigation.pop();
-          const navigate = CommonActions.navigate({
-            name: 'CommonProfile',
-            params: {
-              showRequestSentModal: true,
-              createdProposalId: proposalId,
-            },
-          });
-
-          if (typeof refreshFeed === 'function') {
-            refreshFeed();
-          }
-
-          navigation.dispatch(navigate);
-        } else {
-          navigation.pop();
-          showErrorPopUp(bottomSheetStore, createRequestToJoinResponse);
-        }*/
-      } catch (e) {
-        //navigation.pop();
-
-        bottomSheetStore.showBottomSheet(BOTTOM_SHEET_TEMPLATES.BACKEND_ERROR, {
-          subTitle: "We couldn't create your proposal",
-          error: e,
-        });
+        navigation.dispatch(navigate);
+      } else {
+        navigation.pop();
+        showErrorPopUp(bottomSheetStore, createRequestToJoinResponse);
       }
+    } catch (e) {
+      navigation.pop();
+      bottomSheetStore.showBottomSheet(BOTTOM_SHEET_TEMPLATES.BACKEND_ERROR, {
+        subTitle: "We couldn't create your proposal",
+        error: e,
+      });
     }
   };
 
   const onMessage = async (event) => {
     console.log('On Message', event);
+  };
+
+  const redirectUser = (event) => {
+    if (!respLink) {
+      if (event.title === 'Google') {
+        console.log('event', event.url);
+        setRespLink(event.url);
+        push();
+      }
+    }
   };
 
   return (
@@ -118,19 +134,23 @@ const PaymentDetailsStep = ({
       skipFirstStep={skipFirstStep}
       isRequestToJoin={true}
       layoutTitle={<MembershipRequest />}
-      requestStepActionButton={
+      /*requestStepActionButton={
         <RequestStepActionButton
           title="Pay Now"
           formStore={paymentFormStore}
           onPress={push}
         />
-      }>
+      }*/
+    >
       <View style={{height: height / 2, width: '90%'}}>
         {
           <WebView
             scalesPageToFit={false}
             source={{uri: iFrameLink}}
             onMessage={(m) => onMessage(m)}
+            onNavigationStateChange={(event) => {
+              redirectUser(event);
+            }}
             onLoadEnd={(syntheticEvent) => {
               Toast.done();
               // update component to be aware of loading status
