daf201a78fd717d7edc5f39dab55ae4377b30004
IlyaDylko <ilyazdylko@gmail.com>
2022-01-27 17:31:16 +0100

    small fix

--- src/Screens/Commons/RequestToJoin/Steps/ContributionStep.js
+++ src/Screens/Commons/RequestToJoin/Steps/ContributionStep.js
@@ -5,7 +5,7 @@ import AmountField from '~/Components/FormFields/AmountField';
 import {colors, text} from '~/Theme';
 import RequestToJoinForm from '~/Components/Forms/RequestToJoinForm';
 import RequestStepActionButton from '../../RequestStepActionButton';
-import {CommonActions} from '@react-navigation/native';
+import {CommonActions, useNavigation} from '@react-navigation/native';
 import MembershipRequest from '../MembershipRequest';
 import RequestStepHeaderTitle from '../RequestStepHeaderTitle';
 import {string, func, bool, object, shape} from 'prop-types';
@@ -27,6 +27,7 @@ const ContributionStep = ({
   uiStore,
   authStore,
 }) => {
+  const navigationR = useNavigation();
   const [isActionBtnHidden, setIsActionBtnHidden] = useState(true);
   const metadata = currCommon.metadata;
   const isMonthly = metadata.contributionType === 'monthly';
@@ -86,19 +87,16 @@ const ContributionStep = ({
       const proposalId = createRequestToJoinResponse.data.id;
 
       navigation.pop();
-      const navigate = CommonActions.navigate({
+      navigation.pop();
+
+      navigation.navigate({
         name: 'CommonProfile',
         params: {
+          currCommon: currCommon,
           showRequestSentModal: true,
           createdProposalId: proposalId,
         },
       });
-
-      if (typeof refreshFeed === 'function') {
-        refreshFeed();
-      }
-
-      navigation.dispatch(navigate);
     } else {
       navigation.pop();
       showErrorPopUp(uiStore.bottomSheetStore, createRequestToJoinResponse);
