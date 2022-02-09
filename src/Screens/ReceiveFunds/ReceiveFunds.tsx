e5454476c7bad84342416a1eefb339b0e5bbe6df
IlyaDylko <ilyazdylko@gmail.com>
2022-02-09 14:43:32 +0100

    receive-funds-upgrade

--- src/Screens/ReceiveFunds/ReceiveFunds.tsx
+++ src/Screens/ReceiveFunds/ReceiveFunds.tsx
@@ -1,11 +1,15 @@
 import React, {useCallback, useState} from 'react';
 import {View, Text, Image, StyleSheet, TouchableOpacity} from 'react-native';
 import BottomSheetModal from '~/Components/BottomSheetModal';
+import {AddBankAccountForm} from '~/Components/Forms/AddBankAccountForm';
 import {ModalAddBankAccount} from '~/Screens/ReceiveFunds/components/ModalAddBankAccount';
 import {colors, font, layout, text} from '~/Theme';
+import {useStore} from '~/Util/hooks/useStore';
 
 export const ReceiveFunds = () => {
   const [isModalVisible, setIsModalVisible] = useState(false);
+  const bankAccountStore = useStore('bankAccountStore');
+  const haveBankAccount = bankAccountStore?.data?.size !== 0;
 
   const addBankAccount = useCallback(() => {
     setIsModalVisible(true);
@@ -17,31 +21,65 @@ export const ReceiveFunds = () => {
     console.log('remove');
   };
 
+  const handleSubmit = async (): Promise<void> => {
+    // onSubmit();
+    setIsModalVisible(false);
+  };
+
   return (
     <View style={styles.container}>
-      <View style={styles.emptyObjectContainer}>
-        <Image
-          style={styles.image}
-          source={require('~/Assets/funds.png')}
-        />
-        <Text style={styles.subTitle}>
-          You must provide bank account details in order to receive funds
-        </Text>
-        <TouchableOpacity
-          style={styles.btn}
-          onPress={addBankAccount}>
-          <Text style={styles.buttonText}>
-            Add Bank Account
+      { !haveBankAccount ? (
+        <View style={styles.containerWithBackground}>
+          <Image
+            style={styles.imageWithBackground}
+            source={require('~/Assets/funds.png')}
+          />
+          <Text style={styles.subTitle}>
+            You must provide bank account details in order to receive funds
           </Text>
-        </TouchableOpacity>
-      </View>
+          <TouchableOpacity
+            style={styles.btn}
+            onPress={addBankAccount}>
+            <Text style={styles.buttonText}>
+              Add Bank Account
+            </Text>
+          </TouchableOpacity>
+        </View>
+      ) : (
+        <View style={styles.containerWithoutBackground}>
+          <Image
+            style={styles.image}
+            source={require('~/Assets/funds.png')}
+          />
+          <View style={styles.bankAccountData}>
+            <View>
+              <Text style={styles.accountDataLable}>ID Number</Text>
+              <Text style={styles.accountDataLable}>Bank Name</Text>
+              <Text style={styles.accountDataLable}>Branch</Text>
+              <Text style={styles.accountDataLable}>Account</Text>
+            </View>
+            <View>
+              <Text style={styles.accountDataText}>849384729</Text>
+              <Text style={styles.accountDataText}>Raif Bank</Text>
+              <Text style={styles.accountDataText}>875</Text>
+              <Text style={styles.accountDataText}>18724394</Text>
+            </View>
+          </View>
+          <Text style={styles.subTitle}>
+            These details are needed in order to transfer funds to your account and vissible to you only.
+          </Text>
+        </View>
+      )}
+
       <BottomSheetModal
         style={{borderRadius: 30}}
         isVisible={isModalVisible}
         onClose={closeModal}>
-          <ModalAddBankAccount
+          <AddBankAccountForm
             onCancel={closeModal}
             onDelete={removeAccount}
+            onSubmit={handleSubmit}
+            isAddingNew={true}
           />
       </BottomSheetModal>
     </View>
@@ -55,7 +93,7 @@ const styles = StyleSheet.create({
     paddingTop: 16,
     paddingHorizontal: 24,
   },
-  emptyObjectContainer: {
+  containerWithBackground: {
     width: '100%',
     ...layout.content,
     paddingHorizontal: 28,
@@ -66,6 +104,12 @@ const styles = StyleSheet.create({
     alignSelf: 'center',
     marginHorizontal: 12,
   },
+  containerWithoutBackground: {
+    width: '100%',
+    paddingBottom: 24,
+    borderRadius: 14,
+    alignItems: 'center',
+  },
   subTitle: {
     ...font.primary.regular,
     ...font.fontSize(2),
@@ -75,7 +119,6 @@ const styles = StyleSheet.create({
   btn: {
     backgroundColor: colors.mainBlue,
     height: 56,
-
     justifyContent: 'center',
     alignItems: 'center',
     borderRadius: 28,
@@ -87,9 +130,34 @@ const styles = StyleSheet.create({
     ...text.buttonblack,
     color: colors.white,
   },
-  image: {
+  imageWithBackground: {
     height: 160,
     width: 160,
   },
+  image: {
+    height: 140,
+    width: 140,
+  },
+  bankAccountData: {
+    flexDirection: 'row',
+    justifyContent: 'space-around',
+    borderColor: colors.grey4,
+    borderWidth: 1,
+    borderRadius: 14,
+    width: '100%',
+    paddingTop: 36,
+    paddingBottom: 27,
+    marginTop: 24,
+  },
+  accountDataLable: {
+    ...font.primary.bold,
+    fontSize: 14,
+    marginBottom: 9,
+  },
+  accountDataText: {
+    ...font.primary.regular,
+    fontSize: 14,
+    marginBottom: 9,
+    lineHeight: 18.5,
+  },
 });
-
