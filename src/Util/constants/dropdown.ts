86aebbe6f00d20ce7d27db4cc310fffdb5f75bdd
Pavel Meyer <p.meyer@kernelics.com>
2022-02-15 17:43:18 +0300

    CM-1054 Added NativePicker for ios and android

---
+++ src/Util/constants/dropdown.ts
@@ -0,0 +1,29 @@
+export const GENDER_OPTIONS = [
+  {
+    value: 0,
+    label: 'Female',
+  },
+  {
+    value: 1,
+    label: 'Male',
+  },
+];
+
+export const BANK_NAMES_OPTIONS = [
+  {
+    value: 'Bank Hapoalim',
+    label: 'Bank Hapoalim',
+  },
+  {
+    value: 'Bank Leumi',
+    label: 'Bank Leumi',
+  },
+  {
+    value: 'Bank Mizrahi-Tefahot',
+    label: 'Bank Mizrahi-Tefahot',
+  },
+  {
+    value: 'Israel Discount Bank',
+    label: 'Israel Discount Bank',
+  },
+];
