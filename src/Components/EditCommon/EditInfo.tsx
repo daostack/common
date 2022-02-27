302b70eff35efa92660d70034dbf606093ec40a5
watchtower314 <moore3.14159@gmail.com>
2022-02-27 12:24:50 +0200

    fix error texts

--- src/Components/EditCommon/EditInfo.tsx
+++ src/Components/EditCommon/EditInfo.tsx
@@ -9,8 +9,8 @@ import {FormikProps} from 'formik';
 
 export const validationSchema = object({
   image: yupString().required(),
-  name: yupString().required().label('The first name'),
-  tagLine: yupString().required().label('The last name'),
+  name: yupString().required().label('Name'),
+  tagLine: yupString().required().label('Tagline'),
   about: yupString(),
 });
 
@@ -76,7 +76,6 @@ const EditInfo = (formik: {formikProps: FormikProps<Values>}): ReactElement => {
         value={values?.about}
         viewStyle={{alignSelf: 'stretch'}}
         label="About"
-        infoLabel="Required"
         placeholderText="About"
         onBlur={handleBlur('about')}
         autoCapitalize="none"
