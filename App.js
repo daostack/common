fc40c8edec8cd8d092cf5f7657d609bf9225fb38
Ilya Dylko <ilyadylko@daostack.io>
2022-03-06 10:28:55 +0100

    ui fixes (#1265)

--- App.js
+++ App.js
@@ -427,6 +427,7 @@ const App = ({rootStore, navigation}) => {
             component={ProposalScreen}
             options={({route, ...rest}) => ({
               headerBackTitleVisible: false,
+              headerTitleAlign: 'center',
               headerLeft: () => (
                 <TouchableOpacity
                   onPress={() =>
--- src/Screens/DisscussionMessages/DiscussionMessagesList.js
+++ src/Screens/DisscussionMessages/DiscussionMessagesList.js
@@ -48,6 +48,15 @@ const DiscussionMessagesList = ({
       return acc;
     }, []);
 
+  setTimeout(() => {
+    // Sometimes that code is executed after we leave the actual screen, so we need that check.
+    if (scrollViewRef?.current) {
+      scrollViewRef.current?.scrollToEnd({
+        animated: true,
+      });
+    }
+  }, 150);
+
   return (
     <View style={[styles.viewContainer]}>
       {msgGroups.length > 0 ? (
