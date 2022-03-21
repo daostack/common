578bfaa04170341143ccb32aedcc6be86a0cb0db
Pavel Meyer <p.meyer@kernelics.com>
2022-03-21 16:38:48 +0300

    CM-1214 Added rules for min contribution

--- src/Stores/FormStores/FundingRequestFormStore.js
+++ src/Stores/FormStores/FundingRequestFormStore.js
@@ -1,8 +1,10 @@
 import FormStore from './FormStore';
+import {proposalRules} from './ValidationRules';
 
 class FundingRequestFormStore extends FormStore {
   constructor() {
     super();
+    this.registerValidationRule(proposalRules.validateMaxAmount);
   }
 }
 
