b22ee5d2da096f3e0550f823ffe36c063b1838ba
Pavel Meyer <p.meyer@kernelics.com>
2022-04-05 12:45:31 +0300

    CM-1238 Added error message for ModalChangeVote

--- src/Screens/Proposals/components/ModalChangeVote.tsx
+++ src/Screens/Proposals/components/ModalChangeVote.tsx
@@ -33,105 +33,128 @@ export const ModalChangeVote = ({
   voteOutcome,
   currentUserPhotoUrl,
   votingProcessState,
-}: Props): ReactElement => (
-  <SafeAreaView
-    style={{
-      ...styles.body,
-      height: 350,
-    }}>
-    <View style={styles.plug} />
-    <View style={styles.content}>
-      <View style={styles.imageContainer}>
-        <Image
-          style={[
-            styles.userImage,
-            {borderColor: VOTE_COLORS_BY_STATUSES[voteOutcome]},
-          ]}
-          source={{uri: currentUserPhotoUrl}}
-          width={70}
-          height={70}
-        />
-      </View>
-      <Text style={styles.title}>Change your vote</Text>
+}: Props): ReactElement => {
+  if (votingProcessState.error) {
+    return (
+      <SafeAreaView
+        style={{
+          ...styles.body,
+          height: 350,
+        }}>
+        <View style={styles.plug} />
+        <View style={styles.content}>
+          <Text style={styles.title}>Vote can't be changed</Text>
+          <Text style={styles.subTitle}>
+            This proposal is ending soon - no members {'\n'} are allowed to
+            change their votes.
+          </Text>
+        </View>
+      </SafeAreaView>
+    );
+  }
 
-      {!votingProcessState.inProgress ? (
-        <View style={styles.btnsContainer}>
-          <TouchableOpacity
-            onPress={() => onVote(VOTE_STATUSES.APPROVED)}
+  return (
+    <SafeAreaView
+      style={{
+        ...styles.body,
+        height: 350,
+      }}>
+      <View style={styles.plug} />
+      <View style={styles.content}>
+        <View style={styles.imageContainer}>
+          <Image
             style={[
-              styles.voteBtn,
-              {
-                ...(voteOutcome === VOTE_STATUSES.APPROVED && {
-                  backgroundColor: VOTE_COLORS_BY_STATUSES[voteOutcome],
-                }),
-              },
-            ]}>
-            <Icon
-              name={VOTE_ICON_BY_STATUSES.approved}
-              size={24}
-              color={
-                voteOutcome === VOTE_STATUSES.APPROVED
-                  ? colors.white
-                  : VOTE_COLORS_BY_STATUSES.approved
-              }
-            />
-          </TouchableOpacity>
-          <TouchableOpacity
-            onPress={() => onVote(VOTE_STATUSES.ABSTAINED)}
-            style={[
-              styles.voteBtn,
-              {
-                ...(voteOutcome === VOTE_STATUSES.ABSTAINED && {
-                  backgroundColor: VOTE_COLORS_BY_STATUSES[voteOutcome],
-                }),
-              },
-            ]}>
-            <Icon
-              name={VOTE_ICON_BY_STATUSES.abstained}
-              size={24}
-              color={
-                voteOutcome === VOTE_STATUSES.ABSTAINED
-                  ? colors.white
-                  : VOTE_COLORS_BY_STATUSES.abstained
-              }
-            />
-          </TouchableOpacity>
-          <TouchableOpacity
-            onPress={() => onVote(VOTE_STATUSES.REJECTED)}
-            style={[
-              styles.voteBtn,
-              {
-                ...(voteOutcome === VOTE_STATUSES.REJECTED && {
-                  backgroundColor: VOTE_COLORS_BY_STATUSES[voteOutcome],
-                }),
-              },
-            ]}>
-            <Icon
-              name={VOTE_ICON_BY_STATUSES.rejected}
-              size={24}
-              color={
-                voteOutcome === VOTE_STATUSES.REJECTED
-                  ? colors.white
-                  : VOTE_COLORS_BY_STATUSES.rejected
-              }
-            />
-          </TouchableOpacity>
+              styles.userImage,
+              {borderColor: VOTE_COLORS_BY_STATUSES[voteOutcome]},
+            ]}
+            source={{uri: currentUserPhotoUrl}}
+            width={70}
+            height={70}
+          />
         </View>
-      ) : (
-        <View
-          style={[
-            styles.loading,
-            {
-              backgroundColor:
-                VOTE_COLORS_BY_STATUSES[votingProcessState.processingVoteType],
-            },
-          ]}>
-          <ActivityIndicator color={colors.white} />
-        </View>
-      )}
-    </View>
-  </SafeAreaView>
-);
+        <Text style={styles.title}>Change your vote</Text>
+
+        {!votingProcessState.inProgress ? (
+          <View style={styles.btnsContainer}>
+            <TouchableOpacity
+              onPress={() => onVote(VOTE_STATUSES.APPROVED)}
+              style={[
+                styles.voteBtn,
+                {
+                  ...(voteOutcome === VOTE_STATUSES.APPROVED && {
+                    backgroundColor: VOTE_COLORS_BY_STATUSES[voteOutcome],
+                  }),
+                },
+              ]}>
+              <Icon
+                name={VOTE_ICON_BY_STATUSES.approved}
+                size={24}
+                color={
+                  voteOutcome === VOTE_STATUSES.APPROVED
+                    ? colors.white
+                    : VOTE_COLORS_BY_STATUSES.approved
+                }
+              />
+            </TouchableOpacity>
+            <TouchableOpacity
+              onPress={() => onVote(VOTE_STATUSES.ABSTAINED)}
+              style={[
+                styles.voteBtn,
+                {
+                  ...(voteOutcome === VOTE_STATUSES.ABSTAINED && {
+                    backgroundColor: VOTE_COLORS_BY_STATUSES[voteOutcome],
+                  }),
+                },
+              ]}>
+              <Icon
+                name={VOTE_ICON_BY_STATUSES.abstained}
+                size={24}
+                color={
+                  voteOutcome === VOTE_STATUSES.ABSTAINED
+                    ? colors.white
+                    : VOTE_COLORS_BY_STATUSES.abstained
+                }
+              />
+            </TouchableOpacity>
+            <TouchableOpacity
+              onPress={() => onVote(VOTE_STATUSES.REJECTED)}
+              style={[
+                styles.voteBtn,
+                {
+                  ...(voteOutcome === VOTE_STATUSES.REJECTED && {
+                    backgroundColor: VOTE_COLORS_BY_STATUSES[voteOutcome],
+                  }),
+                },
+              ]}>
+              <Icon
+                name={VOTE_ICON_BY_STATUSES.rejected}
+                size={24}
+                color={
+                  voteOutcome === VOTE_STATUSES.REJECTED
+                    ? colors.white
+                    : VOTE_COLORS_BY_STATUSES.rejected
+                }
+              />
+            </TouchableOpacity>
+          </View>
+        ) : (
+          <View
+            style={[
+              styles.loading,
+              {
+                backgroundColor:
+                  VOTE_COLORS_BY_STATUSES[
+                    votingProcessState.processingVoteType
+                  ],
+              },
+            ]}>
+            <ActivityIndicator color={colors.white} />
+          </View>
+        )}
+      </View>
+    </SafeAreaView>
+  );
+};
 
 const styles = StyleSheet.create({
   body: {
@@ -141,27 +164,6 @@ const styles = StyleSheet.create({
     paddingTop: 0,
     alignItems: 'center',
     width: '100%',
-  },
-  background: {
-    width: '100%',
-  },
-  voteBtn: {
-    height: 48,
-    width: 100,
-    borderRadius: 100,
-    justifyContent: 'center',
-    alignItems: 'center',
-    backgroundColor: colors.white,
-    borderColor: colors.grey4,
-    borderWidth: 1,
-    shadowColor: 'rgba(0, 26, 54, 0.08)',
-    shadowOffset: {
-      width: 0,
-      height: 3,
-    },
-    shadowRadius: 8,
-    shadowOpacity: 1,
-    elevation: 4,
   },
   plug: {
     backgroundColor: colors.paleblue,
@@ -181,14 +183,6 @@ const styles = StyleSheet.create({
     height: 70,
     marginBottom: 26,
   },
-  imageApprove: {
-    height: 70,
-    width: 70,
-    borderWidth: 6,
-    borderRadius: 70,
-    borderColor: colors.lightishGreen,
-    alignSelf: 'center',
-  },
   userImage: {
     height: 70,
     width: 70,
@@ -205,45 +199,36 @@ const styles = StyleSheet.create({
     ...font.primary.bold,
   },
   subTitle: {
+    marginTop: 8,
     fontSize: 16,
-    alignSelf: 'center',
-    marginBottom: 24,
+    textAlign: 'center',
+    lineHeight: 19.2,
+    letterSpacing: 0.16,
+    color: colors.black,
     ...font.primary.regular,
   },
-  btnActionText: {
-    color: colors.white,
-    fontSize: 16,
-    ...font.primary.regular,
-  },
-  btnCancelText: {
-    fontSize: 16,
-    ...font.primary.regular,
-  },
-  btnApprove: {
-    backgroundColor: colors.lightishGreen,
-    marginBottom: 16,
-  },
-  btnReject: {
-    backgroundColor: colors.pinkishOrange,
-    marginBottom: 16,
-  },
-  cancelBtn: {
-    marginBottom: 40,
-  },
-  iconStyle: {
-    height: 116,
-    width: 116,
-    position: 'absolute',
-    alignSelf: 'center',
-    left: 41,
-    bottom: -8,
-  },
-  btnApproveIcon: {},
-  btnRejectIcon: {},
   btnsContainer: {
     flexDirection: 'row',
     justifyContent: 'space-between',
     marginTop: 40,
+  },
+  voteBtn: {
+    height: 48,
+    width: 100,
+    borderRadius: 100,
+    justifyContent: 'center',
+    alignItems: 'center',
+    backgroundColor: colors.white,
+    borderColor: colors.grey4,
+    borderWidth: 1,
+    shadowColor: 'rgba(0, 26, 54, 0.08)',
+    shadowOffset: {
+      width: 0,
+      height: 3,
+    },
+    shadowRadius: 8,
+    shadowOpacity: 1,
+    elevation: 4,
   },
   loading: {
     marginTop: 40,
