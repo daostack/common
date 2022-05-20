34e1fbace1ea313f7252d89380b8967bda5ece77
Pavel Meyer <meyerpavel1207@gmail.com>
2022-05-20 12:46:31 +0300

    CM-1345 Added space to New Discussion page title

--- App.tsx
+++ App.tsx
@@ -649,6 +649,7 @@ const App = () => {
               headerBackTitleVisible: false,
               headerTitleAlign: 'center',
               headerLeft: null,
+              title: 'New Discussion',
               headerRight: () => (
                 <TouchableOpacity
                   style={styles.buttonRight}
