b924cec90dcbb51077991c8431e4a010acb15029
watchtower314 <moore3.14159@gmail.com>
2022-06-08 18:53:58 +0300

    fix some tests

--- src/Components/Commons/CommonBox/CommonBoxCounterBar.test.tsx
+++ src/Components/Commons/CommonBox/CommonBoxCounterBar.test.tsx
@@ -1,7 +1,7 @@
 import React from 'react';
 import {render} from '@testing-library/react-native';
 import {CommonBoxCounterBar} from '~/Components/Commons/CommonBox/CommonBoxCounterBar';
-import {commonMock} from '__mocks__/commonMock';
+import {commonMock} from '../../../../__mocks__/commonMock';
 
 jest.mock('react-native-safe-area-context', () => ({
   useSafeAreaInsets: () => ({insets: null}),
