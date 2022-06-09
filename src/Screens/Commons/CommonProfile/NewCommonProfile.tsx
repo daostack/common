4581abf50894ef3d4b45fd9d8139edb540452329
watchtower314 <moore3.14159@gmail.com>
2022-06-09 15:33:05 +0300

    fix crash

--- src/Screens/Commons/CommonProfile/NewCommonProfile.tsx
+++ src/Screens/Commons/CommonProfile/NewCommonProfile.tsx
@@ -56,7 +56,7 @@ export const NewCommonProfile = observer(() => {
   )!;
   const commonId = currCommon?.id;
   const [hasPermission, setHasPermission] = useState(
-    authStore.getPermission(commonId, authStore.userInfo.uid),
+    authStore.getPermission(commonId, authStore?.userInfo?.uid),
   );
   const bottomSheetStore = uiStore.bottomSheetStore;
   const hasImages = false;
