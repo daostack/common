44471b341ae6f6a5860160514563edf5997f9a72
watchtower314 <moore3.14159@gmail.com>
2022-06-09 21:55:01 +0300

    fix some tests

--- __mocks__/jest.setup.js
+++ __mocks__/jest.setup.js
@@ -8,6 +8,10 @@ jest.mock('@react-native-async-storage/async-storage', () =>
   require('@react-native-async-storage/async-storage/jest/async-storage-mock'),
 );
 
+jest.mock('react-native-share', () => ({
+  default: jest.fn(),
+}));
+
 jest.mock('rn-fetch-blob', () => ({
   fs: {
     dirs: {
