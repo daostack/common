4e3d9a5cbb6b4bfd03a98a3438e3b712413d2c9e
watchtower314 <moore3.14159@gmail.com>
2022-07-31 14:20:08 +0300

    fix privacy policy

--- src/Util/constants/links.ts
+++ src/Util/constants/links.ts
@@ -1,6 +1,6 @@
 export const LINKS = {
   CONTACT_US: 'mailto:hi@common.io',
   HELP: 'https://old.common.io/help',
-  PRIVACY: 'https://old.common.io/privacy',
-  TERMS: 'https://old.common.io/terms',
+  PRIVACY: 'https://common.io/privacy_policy.pdf',
+  TERMS: 'https://common.io/terms_and_conditions.pdf',
 };
